1402cca91f985ecd3e481930ebf9fdd20f3480ab
Sofie Van Den Abeele <sofievandenabeele@MacBook-Pro-van-Sofie.local>
2023-05-08 20:42:00 +0200

    UploadForm Component to set image or set error

--- photo-app/src/comps/UploadForm.jsx
+++ photo-app/src/comps/UploadForm.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+
 const UploadForm = () => {
 
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
 
-    
+    //make an array of allowed types of images
+    const types = ['image/png', 'image/jpeg', 'image/jpg'];
+
     const changeHandler = (e) => {
         let selected = e.target.files[0];
-        console.log(selected);
 
+        //if we have a file & type is correct:update state of file and pass in the selected file
+        if (selected && types.includes(selected.type)) {           
+            setFile(selected);
+            setError('');
+        } else {
+            //reset the value because something invalled was oploaded or not selected 
+            setFile(null);
+            setError('Please select an image (png, jpeg or jpg)');
+        }
     }
 
     return (
         <form>
+            <label>
             <input type="file" onChange={changeHandler}/>
+            <span>+</span>
+            </label>
+            {/* output file if we have a valid file or output error */}
+            <div className="output">
+                { error && <div className="error">{ error }</div> }
+                { file && <div>{ file.name }</div> }
+            </div>
         </form>
     )
 }
